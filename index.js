f2f88149e9eb8a5002f5f84e7c425fecb763d99d
Jonathan Landicheff <jonathan.landicheff@tech4team.fr>
2018-06-30 19:33:29 +0200

    Add debug console messages

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ module.exports = function(sails) {
   const say     = require('./lib/say.js'),
         install = require('./lib/install.js'),
         notify  = require('./lib/notify.js'),
-        init    = require('./lib/init.js');
+        init    = require('./lib/init.js'),
+        shared  = require('./lib/shared.js');
 
   gladys.on('ready', function() {
+    sails.log.debug('Intialising Sonos Say');
     init().catch(sails.log.warn);
   });
 
   return {
     say: say,
     install: install,
-    notify: notify
+    notify: notify,
+    shared: shared
   };
 };
